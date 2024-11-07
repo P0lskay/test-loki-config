330339a9c527c718c77149ffa2eba5f8b4c9c91b
an.makarov <gg.lol.2264@gmail.com>
2024-11-07 09:11:08 +0300

    Make features: configmap_add_data, wait_for_deployment_complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kubernetes import client, config
 import logging
 import yaml
+import time
 
 ### logging construction ###############
 clogger = logging.getLogger(__name__)
@@ -13,6 +14,26 @@ clogger.setLevel(logging.INFO)
 # Load kube config
 config.load_kube_config()
 
+
+def wait_for_deployment_complete(deployment_name, ns, timeout=360):
+    clogger.info(f'Update deployment:{deployment_name}')
+    v1apps = client.AppsV1Api()
+    start = time.time()
+    while time.time() - start < timeout:
+        time.sleep(2)
+        response = v1apps.read_namespaced_deployment_status(deployment_name, ns)
+        s = response.status
+        if (s.updated_replicas == response.spec.replicas and
+                s.replicas == response.spec.replicas and
+                s.available_replicas == response.spec.replicas and
+                s.observed_generation >= response.metadata.generation):
+            return True
+        else:
+            clogger.info(f'[updated_replicas:{s.updated_replicas},replicas:{s.replicas}'
+                  ',available_replicas:{s.available_replicas},observed_generation:{s.observed_generation}] waiting...')
+
+    raise RuntimeError(f'Waiting timeout for deployment {deployment_name}')
+
 def configmap_add_data(cm_name, cm_ns, path_new_data):
     """Add some data to configMap
 
@@ -21,6 +42,18 @@ def configmap_add_data(cm_name, cm_ns, path_new_data):
         cm_ns (str): the namespace of configMap that needs to be updated
         path_new_data (str): the path to the data to be added to ConfigMap
     """
+
+    v1 = client.CoreV1Api()
+
+    configmap = v1.read_namespaced_config_map(cm_name, cm_ns)
+
+    with open(path_new_data, 'r') as f:
+        new_data = yaml.safe_load(f)
+
+    configmap.data.update(new_data)
+
+    v1.replace_namespaced_config_map(cm_name, cm_ns, configmap)
+
 
 def restart_deployments(deployments_lst, ns):
     """Restart deployments and wait for it to be ready
