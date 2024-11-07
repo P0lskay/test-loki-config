4ee34e2e4cdfee57c789e7be511065d803314c91
an.makarov <an.makarov@tensor.ru>
2024-11-07 10:59:13 +0300

    Realize features and tested it: restart_deployments, configmap_add_data

---
+++ helpers.py
@@ -0,0 +1,12 @@
+# Merge two dicts
+def merge_dicts(dict1, dict2):
+    merged = dict1.copy()
+    for key, value in dict2.items():
+        if key in merged:
+            if isinstance(merged[key], dict) and isinstance(value, dict):
+                merged[key] = merge_dicts(merged[key], value)
+            else:
+                merged[key] = value
+        else:
+            merged[key] = value
+    return merged
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from kubernetes import client, config
 import logging
 import yaml
 import time
+import sys
+import datetime
+
+from kubernetes.client import ApiException
+
+from helpers import merge_dicts
 
 ### logging construction ###############
 clogger = logging.getLogger(__name__)
@@ -14,14 +20,12 @@ clogger.setLevel(logging.INFO)
 # Load kube config
 config.load_kube_config()
 
-
-def wait_for_deployment_complete(deployment_name, ns, timeout=360):
+def wait_for_deployment_complete(v1_apps, deployment_name, ns, timeout=360):
     clogger.info(f'Update deployment:{deployment_name}')
-    v1apps = client.AppsV1Api()
     start = time.time()
     while time.time() - start < timeout:
         time.sleep(2)
-        response = v1apps.read_namespaced_deployment_status(deployment_name, ns)
+        response = v1_apps.read_namespaced_deployment_status(deployment_name, ns)
         s = response.status
         if (s.updated_replicas == response.spec.replicas and
                 s.replicas == response.spec.replicas and
@@ -42,26 +46,44 @@ def configmap_add_data(cm_name, cm_ns, path_new_data):
         cm_ns (str): the namespace of configMap that needs to be updated
         path_new_data (str): the path to the data to be added to ConfigMap
     """
-
     v1 = client.CoreV1Api()
 
     configmap = v1.read_namespaced_config_map(cm_name, cm_ns)
+    configmap_data = yaml.safe_load(configmap.data['config.yaml'])
 
     with open(path_new_data, 'r') as f:
-        new_data = yaml.safe_load(f)
+        new_configmap_data = yaml.safe_load(f)
 
-    configmap.data.update(new_data)
-
+    merged_cm_data = merge_dicts(configmap_data, new_configmap_data)
+    configmap.data['config.yaml'] = yaml.dump(merged_cm_data)
     v1.replace_namespaced_config_map(cm_name, cm_ns, configmap)
 
 
-def restart_deployments(deployments_lst, ns):
+def restart_deployments(v1_apps, deployment, ns):
     """Restart deployments and wait for it to be ready
 
     Args:
-        deployments_lst (list): list with the names of deployments to restart
-        ns (_type_): the namespace of deplyments
+        v1_apps (object): k8s api object
+        deployment (string): the name of deployment to restart
+        ns (string): the namespace of deplyments
     """
+    now = datetime.datetime.utcnow()
+    now = str(now.isoformat("T") + "Z")
+    body = {
+        'spec': {
+            'template': {
+                'metadata': {
+                    'annotations': {
+                        'kubectl.kubernetes.io/restartedAt': now
+                    }
+                }
+            }
+        }
+    }
+    try:
+        v1_apps.patch_namespaced_deployment(deployment, ns, body, pretty='true')
+    except ApiException as e:
+        print("Exception when calling AppsV1Api->read_namespaced_deployment_status: %s\n" % e)
 
 def send_logs_query_to_loki(query):
     """Send log request to loki
@@ -83,3 +105,6 @@ def prepare_test_results():
 
 if __name__ == "__main__" :
     clogger.info("Start script")
+    v1_apps = client.AppsV1Api()
+    #restart_deployments(v1_apps, "loki-loki-distributed-querier", "system-logging-new")
+    #wait_for_deployment_complete(v1_apps, "loki-loki-distributed-querier", "system-logging-new")
